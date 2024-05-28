629a0823f1d149ab8a1c04f3b32221bef21ad963
leydi montero <leidy_940@hotmail.es>
2024-05-27 19:51:49 -0500

    implementacion de rutas, links

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+
 
 
 
@@ -9,13 +15,20 @@ import { AboutPageComponent } from './pages/about-page/about-page.component';
   declarations: [
     HomePageComponent,
     AboutPageComponent,
+    SidebarComponent,
+    ContactPageComponent,
+    SearchBoxComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     HomePageComponent,
-    AboutPageComponent
+    AboutPageComponent,
+    SidebarComponent,
+    ContactPageComponent,
+    SearchBoxComponent
   ]
 })
 export class SharedModule { }
