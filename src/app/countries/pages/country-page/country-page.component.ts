b5f23f79ba5a6c3d2b603ca4322898332d1dab8e
leydi montero <leidy_940@hotmail.es>
2024-05-31 08:04:56 -0500

    funcionalidad del link info

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,28 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/contries.service';
+import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
+
 
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
-  styles: ``
+  styles: [
+    `img{
+      padding: 0.25rem;
+      background-color: #fff;
+      border: 1px solid #dee2e6;
+      border-radius: 0.25rem;
+      max-width: 100%;
+      height: auto;
+    }`
+  ]
 })
 export class CountryPageComponent implements OnInit{
 
-   constructor(
+  public country?: Country;
+
+  constructor(
     private activateRoute: ActivatedRoute,
+    private router: Router,
     private CountriesService: CountriesService,
   ) {}
     ngOnInit(): void {
       this.activateRoute.params
-      .subscribe( ({ id })=> {
+      .pipe(
+        switchMap( ({id}) => this.CountriesService.searchCountryByAlphaCode( id ) )
+      )
+      .subscribe( ( country )=> {
 
-        this.CountriesService.searchCountryByAlphaCode( id )
-        .subscribe( country => {
-          console.log({ country });
+        if ( !country ) return this.router.navigateByUrl('');
 
-        })
+        return this.country = country
+
+
       })
   }
 }
--- src/app/countries/services/contries.service.ts
+++ src/app/countries/services/contries.service.ts
@@ -1,7 +1,7 @@
 
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, delay, map, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -11,37 +11,37 @@ export class CountriesService {
 
   constructor(private http: HttpClient) { }
 
-  searchCountryByAlphaCode( code: string ): Observable<Country[]> {
-    const url = `${ this.apiUrl }/alpha/${ code }`
+  private getCountriesRequest( url: string ): Observable<Country[]> {
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( () => of( [] )),
+        delay( 2000 )
+      );
+  }
+
+  searchCountryByAlphaCode( code: string ): Observable<Country | null> {
+    const url = `${ this.apiUrl }/alpha/${ code }`;
     return this.http.get<Country[]>( url )
     .pipe(
-      catchError( () => of( [] ))
+      map( countries => countries.length > 0 ? countries[0]: null),
+      catchError( () => of( null ))
     );
 
   }
 
   searchCapital( term: string): Observable<Country[]> {
     const url = `${ this.apiUrl }/capital/${ term }`
-    return this.http.get<Country[]>( url )
-    .pipe(
-      catchError( () => of( [] ))
-    );
+    return this.getCountriesRequest( url )
   }
 
   searchCountry( term: string ): Observable<Country[]> {
     const url = `${ this.apiUrl }/name/${ term }`
-    return this.http.get<Country[]>( url )
-    .pipe(
-      catchError( () => of( [] ))
-    );
+    return this.getCountriesRequest( url )
   }
 
   searchRegion( region: string ): Observable<Country[]> {
     const url = `${ this.apiUrl }/region/${ region }`
-    return this.http.get<Country[]>( url )
-    .pipe(
-      catchError( () => of( [] ))
-    );
+    return this.getCountriesRequest( url )
   }
 
 }
